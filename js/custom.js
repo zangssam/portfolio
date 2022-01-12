bd285d2ffbca2fcfad4682f4dfdaa47b207e60f2
zangssam <zang.t0919@gmail.com>
2022-01-12 22:42:16 +0900

    애니메이션 수정 및 수치변경
    애니메이션의 숫자 애니메이션 제거 후 css로 전환 효과를 주었습니다.
    percent_bar_03의 수치값 변경하였습니다.

--- js/custom.js
+++ js/custom.js
@@ -10,79 +10,79 @@ $(document).ready(function(){
             $(".skill_list .percent_bar p").addClass("on")
         }
         
-        if(skill_width = scrP){
-            $({value: 0}).animate({value: 80},{duration: 2000,
-                step: function(){
-                $(".skill_list .percent_bar_01 p").text(Math.floor(this.value)+"%");
-            },
-            complete: function(){
-                $(".skill_list .percent_bar_01 p").text(Math.floor(this.value)+"%")
-            } 
-            })
+        // if(skill_width = scrP){
+        //     $({value: 0}).animate({value: 80},{duration: 2000,
+        //         step: function(){
+        //         $(".skill_list .percent_bar_01 p").text(Math.floor(this.value)+"%");
+        //     },
+        //     complete: function(){
+        //         $(".skill_list .percent_bar_01 p").text(Math.floor(this.value)+"%")
+        //     } 
+        //     })
         
-            $({value: 0}).animate({value: 90},{duration: 2000,
-                step: function(){
-                $(".skill_list .percent_bar_02 p").text(Math.floor(this.value)+"%");
-            },
-            complete: function(){
-                $(".skill_list .percent_bar_02 p").text(Math.floor(this.value)+"%")
-            } 
-            })
+        //     $({value: 0}).animate({value: 90},{duration: 2000,
+        //         step: function(){
+        //         $(".skill_list .percent_bar_02 p").text(Math.floor(this.value)+"%");
+        //     },
+        //     complete: function(){
+        //         $(".skill_list .percent_bar_02 p").text(Math.floor(this.value)+"%")
+        //     } 
+        //     })
         
-            $({value: 0}).animate({value: 60},{duration: 2000,
-                step: function(){
-                $(".skill_list .percent_bar_03 p").text(Math.floor(this.value)+"%");
-            },
-            complete: function(){
-                $(".skill_list .percent_bar_03 p").text(Math.floor(this.value)+"%")
-            } 
-            })
+        //     $({value: 0}).animate({value: 70},{duration: 2000,
+        //         step: function(){
+        //         $(".skill_list .percent_bar_03 p").text(Math.floor(this.value)+"%");
+        //     },
+        //     complete: function(){
+        //         $(".skill_list .percent_bar_03 p").text(Math.floor(this.value)+"%")
+        //     } 
+        //     })
         
-            $({value: 0}).animate({value: 85},{duration: 2000,
-                step: function(){
-                $(".skill_list .percent_bar_04 p").text(Math.floor(this.value)+"%");
-            },
-            complete: function(){
-                $(".skill_list .percent_bar_04 p").text(Math.floor(this.value)+"%")
-            } 
-            })
+        //     $({value: 0}).animate({value: 85},{duration: 2000,
+        //         step: function(){
+        //         $(".skill_list .percent_bar_04 p").text(Math.floor(this.value)+"%");
+        //     },
+        //     complete: function(){
+        //         $(".skill_list .percent_bar_04 p").text(Math.floor(this.value)+"%")
+        //     } 
+        //     })
         
-            $({value: 0}).animate({value: 80},{duration: 2000,
-                step: function(){
-                $(".skill_list .percent_bar_05 p").text(Math.floor(this.value)+"%");
-            },
-            complete: function(){
-                $(".skill_list .percent_bar_05 p").text(Math.floor(this.value)+"%")
-            } 
-            })
+        //     $({value: 0}).animate({value: 80},{duration: 2000,
+        //         step: function(){
+        //         $(".skill_list .percent_bar_05 p").text(Math.floor(this.value)+"%");
+        //     },
+        //     complete: function(){
+        //         $(".skill_list .percent_bar_05 p").text(Math.floor(this.value)+"%")
+        //     } 
+        //     })
         
-            $({value: 0}).animate({value: 95},{duration: 2000,
-                step: function(){
-                $(".skill_list .percent_bar_06 p").text(Math.floor(this.value)+"%");
-            },
-            complete: function(){
-                $(".skill_list .percent_bar_06 p").text(Math.floor(this.value)+"%")
-            } 
-            })
+        //     $({value: 0}).animate({value: 95},{duration: 2000,
+        //         step: function(){
+        //         $(".skill_list .percent_bar_06 p").text(Math.floor(this.value)+"%");
+        //     },
+        //     complete: function(){
+        //         $(".skill_list .percent_bar_06 p").text(Math.floor(this.value)+"%")
+        //     } 
+        //     })
         
-            $({value: 0}).animate({value: 95},{duration: 2000,
-                step: function(){
-                $(".skill_list .percent_bar_07 p").text(Math.floor(this.value)+"%");
-            },
-            complete: function(){
-                $(".skill_list .percent_bar_07 p").text(Math.floor(this.value)+"%")
-            } 
-            })
+        //     $({value: 0}).animate({value: 95},{duration: 2000,
+        //         step: function(){
+        //         $(".skill_list .percent_bar_07 p").text(Math.floor(this.value)+"%");
+        //     },
+        //     complete: function(){
+        //         $(".skill_list .percent_bar_07 p").text(Math.floor(this.value)+"%")
+        //     } 
+        //     })
         
-            $({value: 0}).animate({value: 85},{duration: 2000,
-                step: function(){
-                $(".skill_list .percent_bar_08 p").text(Math.floor(this.value)+"%");
-            },
-            complete: function(){
-                $(".skill_list .percent_bar_08 p").text(Math.floor(this.value)+"%")
-            } 
-            })
-        }
+        //     $({value: 0}).animate({value: 85},{duration: 2000,
+        //         step: function(){
+        //         $(".skill_list .percent_bar_08 p").text(Math.floor(this.value)+"%");
+        //     },
+        //     complete: function(){
+        //         $(".skill_list .percent_bar_08 p").text(Math.floor(this.value)+"%")
+        //     } 
+        //     })
+        // }
 
     })
 
